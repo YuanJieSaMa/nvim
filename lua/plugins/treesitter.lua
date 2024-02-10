eaeaf9c45256c5a4fb50f29436fb7912fa5cd543
yuanjie <hy104556222@gmail.com>
2024-02-10 00:31:20 +0000

    add the incremental_selection of treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,17 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   config = function()
-    require 'nvim-treesitter.configs'.setup({})
+    require 'nvim-treesitter.configs'.setup({
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          node_incremental = "v",
+          node_decremental = "<BS>",
+        },
+      },
+      highlight = {
+        enable = true,
+      },
+    })
   end
 }
